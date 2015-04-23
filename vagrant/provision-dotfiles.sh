674948ec7a7661836f71aaf51647e09db888694f
mtbvang <mtb.vang@gmail.com>
2015-04-23 12:09:13 +0200

    Corrected file name.

--- vagrant/provision-dotfiles.sh
+++ vagrant/provision-dotfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cp /vagrant/vagrant/.bashrc ~/
-cp /vagrant/vagrant/bash_completion /etc/bash_completions
+cp /vagrant/vagrant/bash_completion /etc/bash_completion
 cp /vagrant/vagrant/bash_completion_usr /usr/share/bash-completion/bash_completion
 cp /vagrant/vagrant/.bash_aliases ~/
