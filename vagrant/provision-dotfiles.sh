daba2abddacdad7d058de4d036e93eb25dcf07b2
mtbvang <mtb.vang@gmail.com>
2015-04-23 11:46:08 +0200

    Added convenience vagrant target. Removed unused provisioning line.

--- vagrant/provision-dotfiles.sh
+++ vagrant/provision-dotfiles.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cp /vagrant/vagrant/git-completion.bash /etc/bash_completion.d
 cp /vagrant/vagrant/.bashrc ~/
 cp /vagrant/vagrant/bash_completion /etc/bash_completions
 cp /vagrant/vagrant/bash_completion_usr /usr/share/bash-completion/bash_completion
